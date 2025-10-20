b59ccb83fe773b5940b9e42f29309bac0933401c
Tariq Ventura <tariqventura@gmail.com>
2025-10-19 22:50:51 -0600

    add migrations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,29 @@
 package main
 
-import "github.com/tariq-ventura/Proyecto-go/internal/router"
+import (
+	"context"
+
+	"github.com/tariq-ventura/Proyecto-go/internal/db"
+	"github.com/tariq-ventura/Proyecto-go/internal/logs"
+	"github.com/tariq-ventura/Proyecto-go/internal/router"
+)
 
 func main() {
+	ctx := context.Background()
+
 	r := &router.Routes{}
 	r.Routes = r.SetupRouter()
 	r.Run()
+
+	database, err := db.NewDatabase(ctx)
+	if err != nil {
+		logs.LogError("Database connection error", map[string]interface{}{"error": err.Error()})
+		panic("app stopped")
+	}
+
+	err = database.TaskMigration(ctx)
+	if err != nil {
+		logs.LogError("Database migration error", map[string]interface{}{"error": err.Error()})
+		panic("app stopped")
+	}
 }
